99bc487b354d17b60e40f798e10878979ab4e82c
HemaSree1519 <khemasree96@gmail.com>
2019-08-27 16:57:32 +0530

    Pass the test adding the required methods and attributes in Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,10 +1,37 @@
+import java.util.List;
+
 public class Player {
     private String name;
-    public Player(String name) {
+    private List<Double> probability;
+    private int totalRuns;
+    private int totalBallsPlayed;
+    private boolean isOut;
+
+    public Player(String name, List<Double> probability, int totalRuns, int totalBallsPlayed, boolean isOut) {
         this.name=name;
+        this.probability=probability;
+        this.totalRuns=totalRuns;
+        this.totalBallsPlayed=totalBallsPlayed;
+        this.isOut=isOut;
     }
 
     public String getName() {
         return name;
+    }
+
+    public List<Double> getPropability() {
+        return probability;
+    }
+
+    public int getTotalRuns() {
+    return totalRuns;
+    }
+
+    public int getTotalBallsPlayed() {
+    return totalBallsPlayed;
+    }
+
+    public boolean getIsOut() {
+    return isOut;
     }
 }
