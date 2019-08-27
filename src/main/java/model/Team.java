93d76fdf7a97e85274e68b1222b3c93f23bb390f
HemaSree1519 <khemasree96@gmail.com>
2019-08-27 17:39:21 +0530

    Add tests for more attributes for Team and pass them

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -2,12 +2,36 @@ package model;
 
 public class Team {
     private String playingTeam;
+    private String oppositionTeam;
+    private int overs;
+    private int wickets;
+    private int runsToWin;
 
-    public Team(String playingTeam) {
+    public Team(String playingTeam,String oppositionTeam, int overs, int wickets, int runsToWin) {
         this.playingTeam = playingTeam;
+        this.oppositionTeam = oppositionTeam;
+        this.wickets = wickets;
+        this.runsToWin = runsToWin;
+        this.overs = overs;
+    }
+
+    public int getOvers() {
+        return overs;
     }
 
     public String getPlayingTeam() {
         return playingTeam;
+    }
+
+    public String getOppositionTeam() {
+        return oppositionTeam;
+    }
+
+    public int getWickets() {
+        return wickets;
+    }
+
+    public int getRunsToWin() {
+        return runsToWin;
     }
 }
--- src/test/java/model/TeamTest.java
+++ src/test/java/model/TeamTest.java
@@ -9,12 +9,32 @@ public class TeamTest {
 
     @Before
     public void setup(){
-        team = new Team("Bengaluru");
+        team = new Team("Bengaluru", "Chennai", 4, 40, 4);
     }
 
     @Test
     public void getPlayingTeamTest(){
         String playingTeam = team.getPlayingTeam();
         assertEquals("Bengaluru", playingTeam);
+    }
+    @Test
+    public void getOppositionTeamTest(){
+        String oppositionTeam = team.getOppositionTeam();
+        assertEquals("Chennai", oppositionTeam);
+    }
+    @Test
+    public void getOversTest(){
+        int overs = team.getOvers();
+        assertEquals(4, overs);
+    }
+    @Test
+    public void getWicketsTest(){
+        int wickets = team.getWickets();
+        assertEquals(40, wickets);
+    }
+    @Test
+    public void getRunsToWinTest(){
+        int runsToWin = team.getRunsToWin();
+        assertEquals(4, runsToWin);
     }
 }
