0d1dfa6f8ff831886ef5fb5183616f851f2b0eaf
HemaSree1519 <khemasree96@gmail.com>
2019-08-27 16:48:10 +0530

    Add a failing test for player with more attributes

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,19 +1,44 @@
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.xml.soap.SOAPPart;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class PlayerTest {
     Player player;
     @Before
     public void setUp() throws Exception {
-        player = new Player("Hema");
+        player = new Player("Hema",Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 93.0), 0, 0, false);
     }
     @Test
     public void getNameTest() {
-        String PlayerName = player.getName();
-        assertEquals("Hema", PlayerName);
+        String playerName = player.getName();
+        assertEquals("Hema", playerName);
+    }
+    @Test
+    public void getProbabilityTest(){
+        List<Double> playerProbablity = player.getPropability();
+        assertEquals(Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 93.0),playerProbablity);
+    }
+    @Test
+    public void getTotalRunsTest() {
+        int playerTotalRuns = player.getTotalRuns();
+        assertEquals(0, playerTotalRuns);
+
+    }
+    @Test
+    public void getTotalBallsPlayedTest() {
+        int playerTotalBallsPlayed = player.getTotalBallsPlayed();
+        assertEquals(0, playerTotalBallsPlayed);
+    }
+    @Test
+    public void getIsOutTest(){
+        boolean playerIsOut = player.getIsOut();
+        assertFalse(playerIsOut);
     }
 }
