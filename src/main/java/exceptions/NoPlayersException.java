a53c9fdcd287d40c1a32b71f942c235268cc3296
HemaSree1519 <khemasree96@gmail.com>
2019-08-28 21:05:45 +0530

    Define exception for no players in the team

---
+++ src/main/java/exceptions/NoPlayersException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class NoPlayersException {
+    private final String message;
+    public NoPlayersException(String message){
+        super();
+        this.message=message;
+    }
+}
